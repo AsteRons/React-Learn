66ca4cebf1c063331b8bcd20b4c79043c27421d8
Maciej Rudnik <maciej.rudnik2@gmail.com>
2020-11-22 11:48:06 +0100

    Assignment Solution

---
+++ base-syntax--assignment-problem/src/UserOutput/UserOutput.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './UserOutput.css';
+
+const userOutput = (props) => {
+    
+    return (
+        <div className="UserOutput">
+             <p>Username: {props.userName}</p>
+             <p>Random text!</p>
+        </div>
+    );
+}
+
+export default userOutput;
