56b41b4733ba512c66d33a61bb0d0f661c72e927
Maciej Rudnik <maciej.rudnik2@gmail.com>
2020-11-23 16:25:06 +0100

    Flexible Lists

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -25,15 +25,22 @@ class App extends Component {
       ]
     } )
   }
+  nameChangedHandler = ( event, id ) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
 
-  nameChangedHandler = (event) => {
-    this.setState( {
-      persons: [
-        { name: 'Max', age: 28 },
-        { name: event.target.value, age: 29 },
-        { name: 'Stephanie', age: 26 }
-      ]
-    } )
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+    
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState( { persons: persons})
   }
   
   deletePersonHandler = (personIndex) => {
@@ -70,7 +77,8 @@ class App extends Component {
             click = {() => this.deletePersonHandler(index)}
             name = {person.name}
             age = {person.age}
-            key = {person.id} />
+            key = {person.id} 
+            changed = {(event) => this.nameChangedHandler(event, person.id)}/>
           })}
       </div>
       );
