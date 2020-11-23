3057690e4fb535fc1a73ffb3cd87b24f309327fa
Maciej Rudnik <maciej.rudnik2@gmail.com>
2020-11-23 16:03:34 +0100

    Lists & Keys

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -6,9 +6,9 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      { name: 'Max', age: 28 },
-      { name: 'Manu', age: 29 },
-      { name: 'Stephanie', age: 26 }
+      { id: 'sfass', name: 'Max', age: 28 },
+      { id: 'gdfds', name: 'Manu', age: 29 },
+      { id: 'asdfa', name: 'Stephanie', age: 26 }
     ],
     otherState: 'some other value',
     showPersons: false
@@ -69,7 +69,8 @@ class App extends Component {
             return <Person 
             click = {() => this.deletePersonHandler(index)}
             name = {person.name}
-            age = {person.age} />
+            age = {person.age}
+            key = {person.id} />
           })}
       </div>
       );
