a3138b8d9a2b4136ec31ab27fb38ad35e3a53c3b
Maciej Rudnik <maciej.rudnik2@gmail.com>
2020-12-30 20:49:12 +0100

    Rendering Adjacent JSX Elements

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -1,36 +1,32 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { Component } from 'react';
 
-//import './Person.css';
+import Aux from '../../../hoc/Auxiliary';
 
-const StyledDiv = styled.div`  
-        width: 60%;
-        margin: 16px auto;
-        border: 1px solid #eeeeee;
-        box-shadow: 0 2px 3px #cccccc;
-        padding: 16px;
-        text-align: center;
-   
+import './Person.css';
 
-    @media (min-width: 500px) {
-            width: '450px';
+
+
+
+class Person extends Component {
+
+    render() {
+        console.log('[Person.js] rendering...');  
+        return (
+            <Aux>
+
+                <p key="i1" onClick={this.props.click}>
+                   I'm a {this.props.name} and I am {this.props.age} years old!
+                </p>,
+                <p key="i2">{this.props.children}</p>,
+                <input
+                key="i3"
+                type = "text" 
+                onChange={this.props.changed} 
+                value={this.props.name}
+                /> 
+           </Aux>
+        );
     }
-    `;
-
-const person = (props) => {
-
-    console.log('[Person.js] rendering...');
-    
-return (
-    // div className="Person" style={style}>
-    <StyledDiv>
-            <p onClick={props.click}>
-            I'm a {props.name} and I am {props.age} years old!
-            </p>
-        <p>{props.children}</p>
-        <input type = "text" onChange={props.changed} value={props.name}/> 
-    </StyledDiv>
-)
 };
 
-export default person;
+export default Person;
