21cd2cce019b4321b82004f7dcfc4a511f22f796
Maciej Rudnik <maciej.rudnik2@gmail.com>
2021-01-12 14:27:00 +0100

    Creating a Responsive Sidedrawer

--- burger-app/src/components/Layout/Layout.js
+++ burger-app/src/components/Layout/Layout.js
@@ -4,11 +4,12 @@ import App from '../../App';
 import Aux from '../../hoc/Auxs'
 import classes from './Layout.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar';
-
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 const layout = ( props ) => (
 
     <Aux>
        <Toolbar/>
+       <SideDrawer/>
         <main className={classes.Content}>
             {props.children}
         </main> 
