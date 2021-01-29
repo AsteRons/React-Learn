af268ccd2797b234c30e91b108818318ea4b4510
Maciej Rudnik <maciej.rudnik2@gmail.com>
2021-01-29 16:49:07 +0100

    fix view checkout

--- burger-app/src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ burger-app/src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -5,18 +5,17 @@ import Button from '../../UI/Button/Button';
 import classes from './CheckoutSummary.css';
 
 const checkoutSummary = (props) => {
-
     return (
-        <div className = {classes.CheckoutSummary}>
+        <div className={classes.CheckoutSummary}>
             <h1>We hope it tastes well!</h1>
             <div style = {{width: '100%', margin: 'auto'}}>
-            <Burger ingredients = {props.ingredients}/>
+             <Burger ingredients={props.ingredients}/>
             </div>
             <Button 
-                btnTyper ="Danger"
+                btnType ="Danger"
                 clicked={props.CheckoutCancelled}>CANCEL</Button>
             <Button 
-                btnTyper ="Success"
+                btnType ="Success"
                 clicked={props.checkoutContinued}>CONTINUE</Button>
         </div>
     );
