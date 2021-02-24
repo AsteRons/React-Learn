0f804ba57a9b6c50c138724aad34e2ff47049740
Maciej Rudnik <maciej.rudnik2@gmail.com>
2021-02-24 09:50:15 +0100

    A Leaner Switch Case Statement

--- redux/src/store/reducers/result.js
+++ redux/src/store/reducers/result.js
@@ -5,6 +5,11 @@ const initialState = {
     results: []
 }
 
+const deleteResult = (state, action) => {
+    const updatedArray = state.results.filter(result => result.id !== action.resultElId);
+    return updateObject(state, {results: updatedArray});
+};
+
 const reducer = (state = initialState, action) => {
 
     switch(action.type) {
@@ -13,8 +18,7 @@ const reducer = (state = initialState, action) => {
         return  updateObject(state, {results: state.results.concat({id: new Date(),value: action.result * 2})})
 
         case actionTypes.DELETE_RESULT:
-            const updatedArray = state.results.filter(result => result.id !== action.resultElId);
-            return updateObject(state, {results: updatedArray})  
+            return deleteResult(state, action);
     }
 
     return state;
